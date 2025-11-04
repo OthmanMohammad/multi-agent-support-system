a272a6c2d61ba55359dc49cf0b6ef97ef041c19b
Mohammad Othman <emranm226@hotmail.com>
2025-11-04 22:05:18 +0200

    feat: export all repositories including agent performance

--- src/database/repositories/__init__.py
+++ src/database/repositories/__init__.py
@@ -1,12 +1,14 @@
 """
-Repository package - data access layer
+Repository package - Data access layer
 """
 from database.repositories.customer_repository import CustomerRepository
 from database.repositories.conversation_repository import ConversationRepository
 from database.repositories.message_repository import MessageRepository
+from database.repositories.agent_performance_repository import AgentPerformanceRepository
 
 __all__ = [
     "CustomerRepository",
     "ConversationRepository",
-    "MessageRepository"
+    "MessageRepository",
+    "AgentPerformanceRepository",
 ]
