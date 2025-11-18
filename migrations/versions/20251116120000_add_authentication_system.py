9dce061948ed33430a6c6e1157656286900be407
Mohammad Othman <Mo@MohammadOthman.com>
2025-11-18 19:05:35 +0200

    fix: Update authentication migration down_revision to fix circular dependency

--- migrations/versions/20251116120000_add_authentication_system.py
+++ migrations/versions/20251116120000_add_authentication_system.py
@@ -21,7 +21,7 @@ from datetime import datetime
 
 # revision identifiers, used by Alembic.
 revision = '20251116120000'
-down_revision = '20251117000000'
+down_revision = '20251116000000'
 branch_labels = None
 depends_on = None
 
