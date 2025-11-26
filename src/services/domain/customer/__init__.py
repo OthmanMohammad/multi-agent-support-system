a5c92cf813eba4da422f730a82472b4354e9dd92
Mohammad Othman <Mo@MohammadOthman.com>
2025-11-26 22:09:56 +0200

    fix: resolve ruff errors in customer domain init - __all__ fixes

--- src/services/domain/customer/__init__.py
+++ src/services/domain/customer/__init__.py
@@ -1,38 +1,39 @@
 """
 Customer domain services package
 """
+
 from src.services.domain.customer.domain_service import CustomerDomainService
-from src.services.domain.customer.specifications import (
-CustomerIsActive,
-WithinRateLimit,
-HasValidPlan,
-CanUpgradePlan,
-CanDowngradePlan,
-)
 from src.services.domain.customer.events import (
-CustomerCreatedEvent,
-CustomerPlanUpgradedEvent,
-CustomerPlanDowngradedEvent,
-RateLimitExceededEvent,
-CustomerBlockedEvent,
+    CustomerBlockedEvent,
+    CustomerCreatedEvent,
+    CustomerPlanDowngradedEvent,
+    CustomerPlanUpgradedEvent,
+    RateLimitExceededEvent,
+)
+from src.services.domain.customer.specifications import (
+    CanDowngradePlan,
+    CanUpgradePlan,
+    CustomerIsActive,
+    HasValidPlan,
+    WithinRateLimit,
 )
 from src.services.domain.customer.validators import CustomerValidators
 
-all = [
-# Service
-"CustomerDomainService",
-# Specifications
-"CustomerIsActive",
-"WithinRateLimit",
-"HasValidPlan",
-"CanUpgradePlan",
-"CanDowngradePlan",
-# Events
-"CustomerCreatedEvent",
-"CustomerPlanUpgradedEvent",
-"CustomerPlanDowngradedEvent",
-"RateLimitExceededEvent",
-"CustomerBlockedEvent",
-# Validators
-"CustomerValidators",
+__all__ = [
+    "CanDowngradePlan",
+    "CanUpgradePlan",
+    "CustomerBlockedEvent",
+    # Events
+    "CustomerCreatedEvent",
+    # Service
+    "CustomerDomainService",
+    # Specifications
+    "CustomerIsActive",
+    "CustomerPlanDowngradedEvent",
+    "CustomerPlanUpgradedEvent",
+    # Validators
+    "CustomerValidators",
+    "HasValidPlan",
+    "RateLimitExceededEvent",
+    "WithinRateLimit",
 ]
